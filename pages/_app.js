a996b7e54f0e578feeb1b4e4369d942a050f4c19
Milan Poudel <milanwebdeveloper1@gmail.com>
2023-01-10 10:36:22 +0545

    feature/side bar and menus

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import "../App.css";
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+`;
+
+const Wrapper = styled.div``;
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>video cards</Wrapper>
+      </Main>
+    </Container>
+  );
 };
 
 export default Home;
